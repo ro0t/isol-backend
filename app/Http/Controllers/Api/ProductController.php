c644226f34cedd86decf65f851f0799944b2abcc
ro0t <adam@hundaskra.is>
2017-10-26 14:37:08 +0200

    A little work on the mock data

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\ResponseController;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends ResponseController {
 
@@ -64,94 +65,20 @@ class ProductController extends ResponseController {
 
     protected function list() {
 
+        $prod = new Product();
+
         return $this->json([
-            'products' => [
-                [
-                    'id' => 'bhc',
-                    'name' => 'BHC',
-                    'image' => 'BHC.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'borvelar',
-                    'name' => '3x Borvélar',
-                    'image' => 'borvélar.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'conturo',
-                    'name' => 'Conturo',
-                    'image' => 'conturo.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'ryksuga-ct-17',
-                    'name' => 'Ryksuga CT-17',
-                    'image' => 'CT17.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'ryksuga-ctl26e',
-                    'name' => 'Ryksuga CTL26E',
-                    'image' => 'CTL26E.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'skrufuvel-cxs',
-                    'name' => 'Skrúfuvél CXS',
-                    'image' => 'CXS.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'dwc',
-                    'name' => 'Negla DWC',
-                    'image' => 'DWC.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'handfraesari-xt-9',
-                    'name' => 'Handfræsari XT-9',
-                    'image' => 'handfræsarar.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'midi-ryksuga',
-                    'name' => 'Ryksuga MIDI',
-                    'image' => 'MIDI.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'handsog-OF1400',
-                    'name' => 'Handsög OF1400',
-                    'image' => 'OF1400.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'handsog-OF1400-med-ryksugu',
-                    'name' => 'Handsög OF1400 + Ryksuga',
-                    'image' => 'OF1400+ryksuga.jpg',
-                    'manufacturer' => 'Festool'
-                ],
-                [
-                    'id' => 'handsog-sett',
-                    'name' => 'Handsögs sett',
-                    'image' => 'OF1400LAND.jpg',
-                    'manufacturer' => 'Festool'
-                ]
-            ]
+            'products' => $prod->all()
         ]);
 
     }
 
     public function detail($id) {
 
+        $prod = new Product();
+
         return $this->json([
-            'product' => [
-                'id' => $id,
-                'name' => 'Handsögs sett',
-                'image' => 'OF1400LAND.jpg',
-                'manufacturer' => 'Festool'
-            ]
+            'product' => $prod->all($id)
         ]);
 
     }
